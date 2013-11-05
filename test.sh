8cb13b7478795a662d01d1cafe515100edb749c2
Mike <mail@mikekotsch.de>
2013-11-05 02:41:05 +0100

    Get a whole line of text from one entered word

--- test.sh
+++ test.sh
@@ -8,28 +8,30 @@ read word
 result=$(grep -B 1 -w $word /Users/mikekotsch/Documents/Programming/snippets/subtitles.srt | head -1)
 
 # Replace comma with dot
-tmp=${result//,/:}
+# tmp=${result//,/:}
+
+echo "Result: $result"
 
 # Split at the arrow
-IFS=' --> ' read -a TSTAMP <<< "$tmp"
+IFS=' --> ' read -a TSTAMP <<< "$result"
 
 # Assign start and end with values
 start="${TSTAMP[0]}"
-echo "Start: $start"
+start_cal=${start//,/:}
+start=${start//,/.}
 
 end="${TSTAMP[3]}"
-echo "End: $end"
+end_cal=${end//,/:}
 
-# start="${TSTAMP[0]}"
-# end="${TSTAMP[3]}"
 
 # Get duration
-t1_sec=`echo $start | sed 's/:/ /g' | awk '{print $1"*3600000+"$2"*60000+"$3"*1000"+$4}' | bc`
-echo "start: $t1_sec"
-t2_sec=`echo $end | sed 's/:/ /g' | awk '{print $1"*3600000+"$2"*60000+"$3"*1000"+$4}' | bc`
-echo "end: $t2_sec"
+t1=`echo $start_cal | sed 's/:/ /g' | awk '{print $1"*3600000+"$2"*60000+"$3"*1000"+$4}' | bc`
+# echo "start: $t1_sec"
+  t2=`echo $end_cal | sed 's/:/ /g' | awk '{print $1"*3600000+"$2"*60000+"$3"*1000"+$4}' | bc`
+# echo "end:   $t2_sec"
 
-difference=$[$t2_sec-$t1_sec]
+difference=$[$t2-$t1]
+echo "Difference: $difference"
 
 _second=$[$difference/1000%60]
 _milli=$[$difference%1000]
@@ -37,6 +39,6 @@ _milli=$[$difference%1000]
 diff="00:00:$_second.$_milli"
 echo "diff: $diff"
 
-#ffmpeg -i /Users/mikekotsch/Documents/Programming/snippets/inception.avi -ss $start -t $diff -vcodec copy -acodec copy test.avi
+ffmpeg -i /Users/mikekotsch/Documents/Programming/snippets/inception.avi -ss $start -t $diff -vcodec copy -acodec copy test.avi
 
-#ffmpeg -i test.avi finished.avi
+ffmpeg -i test.avi finished.avi
